ddbcdb9819e0ec1fdd88dac9eac07ddb0abd7700
ewasince <ewasince@gmail.com>
2023-12-16 00:29:47 +0300

    task1

---
+++ task1/main.py
@@ -0,0 +1,17 @@
+import logging
+import os
+
+logging.basicConfig(
+    filename='logs.txt',
+    format='%(levelname)s:\t%(message)s',
+    level=logging.INFO
+)
+
+
+def factorial(n: int):
+    return n if n == 1 else n * factorial(n - 1)
+
+
+number = int(os.getenv("FACTORIAL_NUMBER", 1))
+result = factorial(number)
+logging.info(f"{result=}")
