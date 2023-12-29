784aa9ff6bb9b7c4e6d070fa2ed3a061754acfa6
ewasince <ewasince@gmail.com>
2023-12-29 11:00:36 +0300

    fix scripts

--- scripts/init_repos.sh
+++ scripts/init_repos.sh
@@ -8,6 +8,7 @@ set -x
 clone_repos=("task1 task2")
 tmp_dir=`readlink -f tmp`
 cur_dir=$PWD
+readme=README.md
 
 if [ -d "$tmp_dir" ]; then
   rm -rf $tmp_dir
@@ -16,23 +17,37 @@ mkdir -p $tmp_dir
 
 for d in $clone_repos
 do
+  # const
   cd $cur_dir
   rep_dir=$tmp_dir/$d
   rep_url="git@gitlab-service:root/$d.git"
-#  rm -rf rep_dir
-#  mkdir -p $rep_dir
+
+  # clone repo
   cd $tmp_dir
   git clone $rep_url
+
+#  # clear repo
+#  cd $d
+#  rm
+
+  # copy readme
+  cd $cur_dir
+  cp $d/$readme $rep_dir
+
+  # push readme to main
+  cd $rep_dir
+  git add .
+  git commit -m "init"
+  git push
+  git checkout -b dev
+
+  # copy project
   cd $cur_dir
   cp $d -r $tmp_dir
 
-#  git init
+  # push project to dev
+  cd $rep_dir
   git add .
-  git commit -m "auto commit" || echo a
-  git push
-#  git remote add origin $rep_url
-#  git push --set-upstream origin master
-#  cp $d -r $tmp_dir
-
-#  cd rep_dir && git init
+  git commit -m "auto commit"
+  git push --set-upstream origin dev
 done
--- scripts/setup_02_runner_setup.sh
+++ scripts/setup_02_runner_setup.sh
@@ -3,4 +3,5 @@ set -e
 set -x
 
 docker exec gitlab cat /etc/gitlab/initial_root_password
+docker exec -it gitlab-runner bash "echo '172.31.0.2 gitlab-service' >> /etc/hosts" # need tests
 docker exec -it gitlab-runner gitlab-runner register
